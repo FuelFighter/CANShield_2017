6b079ba63691d006c188a8d7826cc59594e208c2
Jørgen Jackwitz <jorgejac@ntnu.no>
2017-04-04 20:51:16 +0200

    Fungerende på Universalmodulene i hvertfall

--- CANShield_2017/main.c
+++ CANShield_2017/main.c
@@ -15,7 +15,6 @@
 #define RX_BUFFER_SIZE 200
 #define CAN_MESSAGE_ID_LENGTH 3
 #define CAN_MESSAGE_LENGTH_LENGTH 1
-#define CAN_MESSAGE_DATA_LENGTH 2
 
 CanMessage_t txFrame;
 CanMessage_t rxFrame;
@@ -23,9 +22,11 @@ char uartTxBuffer[50];
 char uartRxBuffer[RX_BUFFER_SIZE];
 int rxIndex = 0;
 int parseIndex = 0;
-int newUartMessage = 0;
+int newUartMessages = 0;
+uint8_t error = 0;
 
 uint8_t ascii_to_dec(char c);
+void handling_error();
 
 int main(void){
 	cli();
@@ -50,7 +51,7 @@ int main(void){
 			 printf("%s", uartTxBuffer);
 		}
 		
-		if (newUartMessage)
+		if (newUartMessages)
 		{
 			uint16_t canId = 0;
 			uint8_t errorIncrement = 0;
@@ -59,107 +60,68 @@ int main(void){
 			//Looking for [ and skips it if we find it, error otherwise
 			while(uartRxBuffer[parseIndex] != '[')
 			{
-				parseIndex = (parseIndex+1)%RX_BUFFER_SIZE;
+				parseIndex = (parseIndex + 1) % RX_BUFFER_SIZE;
 				if (errorIncrement == RX_BUFFER_SIZE)
 				{
-					printf("ERROR, Could not find CANmessage in uartRxBuffer");
+					printf("ERROR, Could not find start of CANmessage in uartRxBuffer\n");
+					handling_error();
 					break;
 				} else if (errorIncrement > 1)
 				{
-					printf("ERROR, Looking for [, increment: %u", errorIncrement-1);
+					printf("ERROR, Looking for [, increment: %u\n", errorIncrement-1);
 				}
 				errorIncrement++;
 			}
-			errorIncrement = 0;
 			
-			// Setting CAN-ID
-			for (int i = CAN_MESSAGE_ID_LENGTH-1; i >= 0; i--)
+			if (error == 0)
 			{
-				canId += (ascii_to_dec(uartRxBuffer[parseIndex]) << 4*i);
-				parseIndex = (parseIndex+1)%RX_BUFFER_SIZE;
-			}
-			txFrame.id = canId;
-			
-			//Looking for : and skips it if we find it, error otherwise
-			while(uartRxBuffer[parseIndex] != ':')
-			{
-				parseIndex = (parseIndex+1)%RX_BUFFER_SIZE;
-				if (errorIncrement == RX_BUFFER_SIZE)
+				parseIndex = (parseIndex + 1) % RX_BUFFER_SIZE;
+				
+				
+				// Setting CAN-ID
+				canId += (ascii_to_dec(uartRxBuffer[parseIndex]) << 8);
+				parseIndex = (parseIndex + 1) % RX_BUFFER_SIZE;
+				canId += (ascii_to_dec(uartRxBuffer[parseIndex]) << 4);
+				parseIndex = (parseIndex + 1) % RX_BUFFER_SIZE;
+				canId += ascii_to_dec(uartRxBuffer[parseIndex]);
+				parseIndex = (parseIndex + 1) % RX_BUFFER_SIZE;
+				printf("CAN id: %03X\n",canId);
+				txFrame.id = canId;
+				
+				//Jump over :
+				parseIndex = (parseIndex + 1) % RX_BUFFER_SIZE;
+				
+				//Translating from ASCII and setting length
+				txFrame.length = ascii_to_dec(uartRxBuffer[parseIndex]);
+				parseIndex = (parseIndex+1) % RX_BUFFER_SIZE;
+				
+				printf("CAN Length: %u\n",txFrame.length);
+				
+				//Jump over :
+				parseIndex = (parseIndex+1) % RX_BUFFER_SIZE;
+				
+				//Setting data
+				for (int i = 0; i < txFrame.length; i++)
 				{
-					printf("ERROR, Could not find CAN length in uartRxBuffer");
-					break;
-				} else if (errorIncrement > 1)
-				{
-					printf("ERROR, Looking for :, increment: %u", errorIncrement-1);
+					canData = (ascii_to_dec(uartRxBuffer[parseIndex]) << 4);
+					parseIndex = (parseIndex+1) % RX_BUFFER_SIZE;
+					
+					canData |= (ascii_to_dec(uartRxBuffer[parseIndex]));
+					parseIndex = (parseIndex+1) % RX_BUFFER_SIZE;
+					
+					txFrame.data[i] = canData;
+					printf("Can Data %d: %02X\n", i, txFrame.data[i]);
 				}
-				errorIncrement++;
+				
+				//Jump over ]
+				parseIndex = (parseIndex+1) % RX_BUFFER_SIZE;
+				//Jump over \n
+				parseIndex = (parseIndex+1) % RX_BUFFER_SIZE;
+				
+				can_send_message(&txFrame);
+				newUartMessages-- ;	
 			}
-			errorIncrement = 0;
-			
-			//Translating from ASCII and setting length
-			txFrame.length = ascii_to_dec(uartRxBuffer[parseIndex]);
-			
-			//Looking for : and skips it if we find it, error otherwise
-			while(uartRxBuffer[parseIndex] != ':')
-			{
-				parseIndex = (parseIndex+1)%RX_BUFFER_SIZE;
-				if (errorIncrement == RX_BUFFER_SIZE)
-				{
-					printf("ERROR, Could not find CAN Data in uartRxBuffer");
-					break;
-				} else if (errorIncrement > 1)
-				{
-					printf("ERROR, Looking for :, increment: %u", errorIncrement-1);
-				}
-				errorIncrement++;
-			}
-			errorIncrement = 0;
-			
-			//Setting data
-			for (int i = 0; i < txFrame.length; i++)
-			{
-				for (int j = (CAN_MESSAGE_DATA_LENGTH-1); j >= 0; j--)
-				{
-					canData += (ascii_to_dec(uartRxBuffer[parseIndex]) << 4*i);
-					parseIndex = (parseIndex+1)%RX_BUFFER_SIZE;
-				}
-				txFrame.data[i] = canData;
-				canData = 0;
-			}
-			
-			//Looking for ] and skips it if we find it, error otherwise
-			while(uartRxBuffer[parseIndex] != ']')
-			{
-				parseIndex = (parseIndex+1)%RX_BUFFER_SIZE;
-				if (errorIncrement == RX_BUFFER_SIZE)
-				{
-					printf("ERROR, Could not find ] in uartRxBuffer");
-					break;
-				} else if (errorIncrement > 1)
-				{
-					printf("ERROR, Looking for ], increment: %u", errorIncrement-1);
-				}
-				errorIncrement++;
-			}
-			errorIncrement = 0;
-			
-			//Looking for \n and skips it if we find it, error otherwise
-			while(uartRxBuffer[parseIndex] != '\n')
-			{
-				parseIndex = (parseIndex+1)%RX_BUFFER_SIZE;
-				if (errorIncrement == RX_BUFFER_SIZE)
-				{
-					printf("ERROR, Could not find end of CAN Data in uartRxBuffer");
-					break;
-				} else if (errorIncrement > 1)
-				{
-					printf("ERROR, Looking for \n, increment: %u", errorIncrement-1);
-				}
-				errorIncrement++;
-			}
-			errorIncrement = 0;
-			can_send_message(&txFrame);
-			newUartMessage = 0;
+			error--;
 		}
 	}
 }
@@ -167,12 +129,11 @@ int main(void){
 ISR(USART0_RX_vect){
 	char rxChar = usbdbg_rx_char();
 	uartRxBuffer[rxIndex] = rxChar;
-	
-	rxIndex = (rxIndex+1)%RX_BUFFER_SIZE;
+	rxIndex = (rxIndex + 1) % RX_BUFFER_SIZE;
 	
 	if (rxChar == '\n')
 	{	
-		newUartMessage = 1;
+		newUartMessages++;
 	}
 }
 
@@ -182,19 +143,24 @@ uint8_t ascii_to_dec(char c)
 	
 	if ((c >= '0') && (c <= '9'))
 	{
-		dec = (uint8_t)(c - '0');
+		dec = c - '0';
 		
 	} else if ((c >= 'A') && (c <= 'F'))
 	{
-		dec = (uint8_t)(c - 'A');
+		dec = c - 'A' + 10;
 		
 	} else if ((c >= 'a') && (c <= 'f'))
 	{
-		dec = (uint8_t)(c - 'a');
+		dec = c - 'a' + 10;
 		
-		} else {
+	} else {
 		dec = 0;
 	}
 	
-	return dec;
+	return (uint8_t)dec;
+}
+
+void handling_error(){
+	error++;
+	newUartMessages--;
 }
